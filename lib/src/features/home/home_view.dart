0b218408e0ca9e0def8d289e68c17557e4317db6
John Manning <john.d.manning@gmail.com>
2023-04-28 13:25:31 -0400

    :sparkles: add new icons as items

--- lib/src/features/home/home_view.dart
+++ lib/src/features/home/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:u_maryland/src/features/home/home.dart';
 
@@ -65,14 +66,76 @@ class _Selections extends HookConsumerWidget {
       child: SingleChildScrollView(
         controller: scrollController,
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 12.0),
+          padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8.0),
           child: Wrap(
+            alignment: WrapAlignment.spaceEvenly,
             spacing: 12,
             runSpacing: 24,
-            children: List.generate(150, (index) => const Text('text')),
+            children: _items,
           ),
         ),
       ),
     );
   }
 }
+
+class _Item extends StatelessWidget {
+  const _Item(
+      {required this.text, required this.onPressed, required this.icon});
+
+  final String text;
+  final void Function()? onPressed;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return OutlinedButton(
+      onPressed: onPressed,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 2.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(icon, size: 48),
+            const Gap(12),
+            Text(
+              text,
+              style: theme.textTheme.titleMedium,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final List<Widget> _items = [
+  _Item(
+      text: 'Calendar', icon: Icons.calendar_month_outlined, onPressed: () {}),
+  _Item(text: 'UMD Dining', icon: Icons.flatware_outlined, onPressed: () {}),
+  _Item(
+      text: 'Directory', icon: Icons.account_circle_outlined, onPressed: () {}),
+  _Item(text: 'Report Issue', icon: Icons.wifi_outlined, onPressed: () {}),
+  _Item(text: 'Map', icon: Icons.map, onPressed: () {}),
+  _Item(text: 'Indoor Maps', icon: Icons.map_outlined, onPressed: () {}),
+  _Item(text: 'Find a Space', icon: Icons.pin_drop_outlined, onPressed: () {}),
+  _Item(
+      text: 'Shuttle-UM',
+      icon: Icons.directions_bus_filled_outlined,
+      onPressed: () {}),
+  _Item(text: 'Get Involved', icon: Icons.inventory_outlined, onPressed: () {}),
+  _Item(
+      text: 'Libraries', icon: Icons.library_books_outlined, onPressed: () {}),
+  _Item(text: 'Courses', icon: Icons.menu_book_outlined, onPressed: () {}),
+  _Item(
+      text: 'Contact Us',
+      icon: Icons.perm_contact_cal_outlined,
+      onPressed: () {}),
+  _Item(text: 'Help Desk', icon: Icons.map_outlined, onPressed: () {}),
+  _Item(text: 'RecWell', icon: Icons.favorite_outline, onPressed: () {}),
+  _Item(text: 'Counseling', icon: Icons.chat_bubble_outline, onPressed: () {}),
+  _Item(text: 'COVID Info', icon: Icons.sick_outlined, onPressed: () {}),
+  _Item(text: 'Police', icon: Icons.local_police_outlined, onPressed: () {}),
+];
